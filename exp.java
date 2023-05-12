7a54bc9a3dad6cd47e94109710d40c3fc748dfa8
ARUNA <131846919+Aruna-65@users.noreply.github.com>
2023-05-12 12:02:34 +0530

    Add files via upload

--- exp.java
+++ exp.java
@@ -1,7 +1,16 @@
 public class exp {
-    {
-        public static void main(String[] args)
-        {
-            System.out.println("");
+    public static void main(String[] args) {
+        int a = 20, b = 0;
+        int c=0;
+        try {
+            c = a / b;
         }
+        catch (Exception e){
+            System.out.println("error occured");
+        }
+        System.out.println(c);
+        System.out.println("End of program");
     }
+}
+
+
